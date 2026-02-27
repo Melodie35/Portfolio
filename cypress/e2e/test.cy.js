9214a8f311b953bd8791494df9d5a6e2161de7ad
Melodie <melodie.migault@gmail.com>
2026-02-27 12:07:28 +0100

    test connexion

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -5,16 +5,6 @@ describe('website connexion', () => {
         cy.visit('/')
     })
 
-    it('gets the home page', () => {
-        cy.getBySel('navlink-name').should('be.visible')
-        cy.getBySel('navlink-home').should('be.visible')
-        cy.getBySel('navlink-about').should('be.visible')
-        cy.getBySel('navlink-portfolio').should('be.visible')
-        cy.getBySel('navlink-contact').should('be.visible')
-        cy.getBySel('hero-h1').should('be.visible')
-        cy.getBySel('hero-h2').should('be.visible')
-    })
-
     it('loads skills.json', () => {
          cy.request({
             method: 'GET',
@@ -33,6 +23,21 @@ describe('website connexion', () => {
         })
     })
 
-    
+    it('gets the home page', () => {
+        cy.getBySel('navlink-name').should('be.visible')
+        cy.getBySel('navlink-home').should('be.visible')
+        cy.getBySel('navlink-about').should('be.visible')
+        cy.getBySel('navlink-portfolio').should('be.visible')
+        cy.getBySel('navlink-contact').should('be.visible')
+        cy.getBySel('hero-h1').should('be.visible')
+        cy.getBySel('hero-h2').should('be.visible')
+    })
+
+    it('gets the sections', () => {
+        cy.getBySel('container-about').should('be.visible')
+        cy.getBySel('container-skills').should('be.visible')
+        cy.getBySel('container-portfolio').should('be.visible')
+        cy.getBySel('container-contact').should('be.visible')
+    })
 
 })
