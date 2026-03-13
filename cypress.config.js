94046b45f29ea3c0c51394e00fc1ccc22185aa97
Melodie <melodie.migault@gmail.com>
2026-03-13 16:18:18 +0100

    add video and report in artifacts

--- cypress.config.js
+++ cypress.config.js
@@ -1,14 +1,18 @@
 import { defineConfig } from "cypress";
-import * as ccModule from '@cypress/code-coverage/task.js'
-const registerCodeCoverageTaks = ccModule.default
 
 export default defineConfig({
   video:true,
   e2e: {
     baseUrl: "http://127.0.0.1:5500",
+    reporter: 'mochawesome',
+    reporterOptions: {
+      reportDir: 'cypress/reports',
+      overwrite: false,
+      html: true,
+      json: true
+    },
     setupNodeEvents(on, config) {
       // implement node event listeners here
-      registerCodeCoverageTaks(on, config)
       return config
     },
   },
