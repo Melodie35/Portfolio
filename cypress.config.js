5defbe8c6258f652335e812fe785edb426532c81
Melodie <melodie.migault@gmail.com>
2026-03-13 14:34:39 +0100

    Coverage report added

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,14 @@
-const { defineConfig } = require("cypress");
+import { defineConfig } from "cypress";
+import * as ccModule from '@cypress/code-coverage/task.js'
+const registerCodeCoverageTaks = ccModule.default
 
 module.exports = defineConfig({
   e2e: {
     baseUrl: "http://127.0.0.1:5500",
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      registerCodeCoverageTaks(on, config)
+      return config
     },
   },
 });
